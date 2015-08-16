e64405dab0aa1a5552604c54c3a93ecd7c7a31b7
Tamara Chahine <tamara@balancedlabs.com>
2015-08-16 16:46:01 +1000

    Add extra classes and checked binding to mdl-checkbox

--- addon/components/mdl-checkbox.js
+++ addon/components/mdl-checkbox.js
@@ -3,7 +3,8 @@ import layout from '../templates/components/mdl-checkbox';
 
 export default Ember.Component.extend({
   layout: layout,
-  classNames: ["mdl-checkbox", "mdl-js-checkbox", "mdl-js-ripple-effect"],
+  classNames: ["mdl-checkbox", "mdl-js-checkbox", "mdl-js-ripple-effect", "is-upgraded", "mdl-js-ripple-effect--ignore-events"],
+  classNameBindings: ["checked:is-checked"],
   tagName: "label",
   attributeBindings: ["for"],
   for: Ember.computed.alias("type"),
